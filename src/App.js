1e6b6103fcb61f995d90922b671471a9f6c0e304
Oktay Arda <hasanoktayarda@gmail.com>
2018-09-26 10:30:29 +0200

    2. ürün ekleme

--- src/App.js
+++ src/App.js
@@ -15,8 +15,15 @@ function App() {
       <h1>My Shop</h1>
       <ShopItem name="Books" />
       <img src="https://images-eu.ssl-images-amazon.com/images/I/61AcOQyLg9L._AC_SY230_.jpg" />
-      <h3>Meine schönsten Kinderlieder </h3>
-      <span style={{ color: "#ff9900" }}>{"★️".repeat(5)}</span> <br />
+      <h4>Meine schönsten Kinderlieder</h4>
+      <span style={{ color: "#ff9900" }}>{"★️".repeat(5)}</span>
+      <br />
+      <span style={{ color: "#cc0000" }}>{"EUR 19,99"}</span>
+      <br />
+      <img src="https://images-eu.ssl-images-amazon.com/images/I/A19RGBZJ8nL._AC_SY230_.jpg" />
+      <h4>Leckerschmecker - Die 100 Größten Hits</h4>
+      <span style={{ color: "#ff9900" }}>{"★️".repeat(3)}</span>
+      <br />
       <span style={{ color: "#cc0000" }}>{"EUR 19,99"}</span>
     </div>
   );
