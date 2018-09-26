d6048cdc220c9b7ed834ad3d3fa9db0cb9f5d7ac
Oktay Arda <hasanoktayarda@gmail.com>
2018-09-26 09:36:00 +0200

    Books komponeneti

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
-import "./App.css";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+function ShopItem(props) {
+  return (
+    <div>
+      <h2>{props.name}</h2>
+      <hr />
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <div>
+      <h1>My Shop</h1>
+      <ShopItem name="Books" />
+    </div>
+  );
 }
 
 export default App;
