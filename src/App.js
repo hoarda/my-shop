f5e29102c0332c2a40372b550fadbec1116eb97b
Oktay Arda <hasanoktayarda@gmail.com>
2018-09-26 10:00:35 +0200

    Fiyat ekleme

--- src/App.js
+++ src/App.js
@@ -16,7 +16,8 @@ function App() {
       <ShopItem name="Books" />
       <img src="https://images-eu.ssl-images-amazon.com/images/I/61AcOQyLg9L._AC_SY230_.jpg" />
       <h3>Meine schönsten Kinderlieder </h3>
-      <span style={{ color: "#ff9900  	" }}>{"★️".repeat(5)}</span>
+      <span style={{ color: "#ff9900" }}>{"★️".repeat(5)}</span> <br />
+      <span style={{ color: "#cc0000" }}>{"EUR 19,99"}</span>
     </div>
   );
 }
